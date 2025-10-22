3b801f3b117bb7c15a3de0c6dba4be067b6a8584
Ishak <ishaksmajicpac@gmail.com>
2025-10-22 16:41:13 +0200

    Added a new object centered around Warhammer merch

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
 // Images not working at this time.
-fetch('machines.json')
-    .then(res => res.json())
-    .then(data => renderProducts(data));
+const products = [
+    {
+        id: "prod1",
+        name: "Dawn of War 1",
+        category: "Videogame",
+        price: 40
+    },
+    {
+        id: "prod2",
+        name: "Dawn of War 3",
+        category: "Videogame",
+        price: 40
+    },
+    {
+        id: "prod3",
+        name: "Dawn of War 3",
+        category: "Videogame",
+        price: 40
+    },
+    {
+        id: "prod4",
+        name: "Dawn of War 4",
+        category: "Videogame",
+        price: 40
+    },
+    {
+        id: "prod5",
+        name: "Dark Imperium",
+        category: "Book",
+        price: 19.99
+    },
+    {
+        id: "prod6",
+        name: "Helsreach",
+        category: "Book",
+        price: 25
+    },
+    {
+        id: "prod7",
+        name: "Black Templar Combat Patrol",
+        category: "Miniature",
+        price: 175
+    }
+]
